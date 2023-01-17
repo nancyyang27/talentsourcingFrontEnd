1c630427abe91efc21a7236aebf8baaf66322c66
nancyyang27 <62917347+nancyyang27@users.noreply.github.com>
2023-01-16 22:33:28 -0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,62 +1,42 @@
-// import type { AppProps } from 'next/app';
-// import { ChakraProvider } from '@chakra-ui/react';
-// import { ApolloProvider } from '@apollo/client';
-// import { useEffect } from 'react';
-// import Amplify, { Auth } from 'aws-amplify';
-// import { createAppSyncClient } from '../appsync/AppSyncClient';
-// import amplifyConfig from '../deployment/amplify-config';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { ApolloProvider } from '@apollo/client';
+import { useEffect } from 'react';
+import Amplify, { Auth } from 'aws-amplify';
+import { createAppSyncClient } from '../appsync/AppSyncClient';
+import amplifyConfig from '../deployment/amplify-config';
 
-// Amplify.configure(amplifyConfig);
+Amplify.configure(amplifyConfig);
 
-// function App({ Component, pageProps, router }: AppProps) {
-//   const validateUserSession = async () => {
-//     try {
-//       await Auth.currentSession();
-//     } catch (error) {
-//       console.error(error);
-//       router.push('/signin');
-//     }
-//   };
+function App({ Component, pageProps, router }: AppProps) {
+  const validateUserSession = async () => {
+    try {
+      await Auth.currentSession();
+    } catch (error) {
+      console.error(error);
+      router.push('/signin');
+    }
+  };
 
-//   const getUserSession = async () => {
-//     try {
-//       await Auth.currentSession();
-//     } catch (error) {
-//       router.push('/signin');
-//     }
-//   };
+  const getUserSession = async () => {
+    try {
+      await Auth.currentSession();
+    } catch (error) {
+      router.push('/signin');
+    }
+  };
 
-//   useEffect(() => {
-//     getUserSession();
-//   }, []);
+  useEffect(() => {
+    getUserSession();
+  }, []);
 
-//   return (
-//     <ChakraProvider>
-//       <ApolloProvider client={createAppSyncClient(validateUserSession)}>
-//         <Component {...pageProps} />
-//       </ApolloProvider>
-//     </ChakraProvider>
-//   );
-// }
-
-// export default App;
-import "@aws-amplify/ui-react/styles.css";
-import { withAuthenticator, Button, Heading, Image, View, Card } from "@aws-amplify/ui-react";
-import React from 'react';
-
-interface Props {
-  signOut: () => void;
-}
-
-const App: React.FC<Props> = ({ signOut }) => {
   return (
-    <View className="App">
-      <Card>
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    <ChakraProvider>
+      <ApolloProvider client={createAppSyncClient(validateUserSession)}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </ChakraProvider>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
